c882bf9a20b892b4c2d478e95800e5960d85ff1e
JI <risma825j@gmail.com>
2022-06-19 12:01:31 +0900

    add api

--- barcode/src/main/java/kr/azazel/barcode/AzAppConfig.java
+++ barcode/src/main/java/kr/azazel/barcode/AzAppConfig.java
@@ -3,8 +3,8 @@ package kr.azazel.barcode;
 public class AzAppConfig {
     public static final String PUBLISHER = "Azazel";
     public static final String NAME = "AzBarcode";
-    public static final String VERSION = "3.0";
-    public static final int VERSION_CODE = 30;
+    public static final String VERSION = "3.1";
+    public static final int VERSION_CODE = 31;
 
     public static final boolean LOG_ENABLED = false;
     public static final boolean FILE_LOG_ENABLED = false;
